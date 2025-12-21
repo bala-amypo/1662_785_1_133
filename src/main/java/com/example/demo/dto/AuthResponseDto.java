d6a74db9507443d593665791153d42c862815b9b
bala-amypo <bala@amypo.in>
2025-12-21 18:18:03 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/AuthResponseDto.java
@@ -0,0 +1,31 @@
+package com.example.demo.dto;
+
+public class AuthResponseDto {
+
+    private String token;
+    private Long expiresAt;
+
+    public AuthResponseDto() {
+    }
+
+    public AuthResponseDto(String token, Long expiresAt) {
+        this.token = token;
+        this.expiresAt = expiresAt;
+    }
+
+    public String getToken() {
+        return token;
+    }
+    
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Long getExpiresAt() {
+        return expiresAt;
+    }
+    
+    public void setExpiresAt(Long expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+}
