3b683b31c4cf5f868b3ab136179ff072b6cac79c
bala-amypo <bala@amypo.in>
2025-12-22 18:08:44 +0000

    User commit

--- src/main/java/com/example/demo/repository/StoreRepository.java
+++ src/main/java/com/example/demo/repository/StoreRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
-
     Optional<Store> findByStoreName(String storeName);
 }
--- src/main/java/com/example/demo/repository/TransferSuggestionRepository.java
+++ src/main/java/com/example/demo/repository/TransferSuggestionRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TransferSuggestionRepository extends JpaRepository<TransferSuggestion, Long> {
-
     List<TransferSuggestion> findBySourceStoreId(Long storeId);
 }
