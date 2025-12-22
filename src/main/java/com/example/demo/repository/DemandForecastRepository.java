470cc607bde9ec2e275b55b86787a758a61ca77f
bala-amypo <bala@amypo.in>
2025-12-22 18:53:44 +0000

    User commit

--- src/main/java/com/example/demo/repository/DemandForecastRepository.java
+++ src/main/java/com/example/demo/repository/DemandForecastRepository.java
@@ -1,12 +1,19 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.*;
+import com.example.demo.entity.DemandForecast;
+import com.example.demo.entity.Store;
+import com.example.demo.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
-public interface DemandForecastRepository extends JpaRepository<DemandForecast, Long> {
+public interface DemandForecastRepository
+        extends JpaRepository<DemandForecast, Long> {
+
     Optional<DemandForecast> findByStoreAndProductAndForecastDateAfter(
-            Store store, Product product, LocalDate date);
+            Store store,
+            Product product,
+            LocalDate date
+    );
 }
--- src/main/java/com/example/demo/repository/InventoryLevelRepository.java
+++ src/main/java/com/example/demo/repository/InventoryLevelRepository.java
@@ -1,18 +1,14 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.DemandForecast;
+import com.example.demo.entity.InventoryLevel;
 import com.example.demo.entity.Store;
 import com.example.demo.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
 import java.util.Optional;
 
-public interface DemandForecastRepository extends JpaRepository<DemandForecast, Long> {
+public interface InventoryLevelRepository
+        extends JpaRepository<InventoryLevel, Long> {
 
-    Optional<DemandForecast> findByStoreAndProductAndForecastDateAfter(
-            Store store,
-            Product product,
-            LocalDate date
-    );
+    Optional<InventoryLevel> findByStoreAndProduct(Store store, Product product);
 }
