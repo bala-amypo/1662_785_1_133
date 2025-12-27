6080835243ae173403486b1f31ffc146f724cbb5
bala-amypo <bala@amypo.in>
2025-12-27 05:37:10 +0000

    User commit

--- src/main/java/com/example/demo/service/StoreService.java
+++
@@ -1,9 +0,0 @@
-@WebServlet("/status")
-public class SimpleStatusServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.setContentType("text/plain");
-        resp.setStatus(HttpServletResponse.SC_OK);
-        resp.getWriter().write("Multi-Location Inventory Balancer is running");
-    }
-}
